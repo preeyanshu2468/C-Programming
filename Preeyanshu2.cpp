78b9656d50852b4f5653bfd0100a2440b460016a
PREEYANSHU RANJAN <63910114+preeyanshu2468@users.noreply.github.com>
2021-06-03 17:15:32 +0530

    Add files via upload

---
+++ Preeyanshu2.cpp
@@ -0,0 +1,10 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter three numbers\n");
+	scanf("%d%d%d", &a,&b,&c);
+	d=a+b+c;
+	printf("sum of %d, %d and %d is %d",a,b,c,d);
+}
