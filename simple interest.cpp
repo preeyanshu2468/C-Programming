6748f12ea484da93e0717d430ece1bd62908dec1
PREEYANSHU RANJAN <63910114+preeyanshu2468@users.noreply.github.com>
2021-06-03 17:02:45 +0530

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,15 @@
+/* Calculation of simple interest*/
+/*Author:gekay Date:25/06/2017*/
+#include<stdio.h>
+int main()
+{
+	int p,n;
+	float r,si;
+	p=1000;
+	n=3;
+	r=8.5;
+	/*formula for simple interest*/
+	si=(p*n*r)/100;
+	printf("%f\n",si);
+	return 0;
+}
