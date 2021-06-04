c575aec95111b076848ef97da41d36bb0c32a5db
PREEYANSHU RANJAN <63910114+preeyanshu2468@users.noreply.github.com>
2021-06-04 11:22:50 +0530

    Add files via upload

---
+++ substract.cpp
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter two numbers\n");
+	scanf("%d %d",&a,&b);
+	c=a-b;
+	printf("substraction of two %d and %d is %d",a,b,c);
+	
+}
